fc67c8b3d12d1164abfac5ba98b53f8e750dfc48
simplex991 <47430299+simplex991@users.noreply.github.com>
2019-03-17 01:18:09 +0100

    Update dbh.inc.php

--- dbh.inc.php
+++ dbh.inc.php
@@ -5,7 +5,7 @@ $version = "#{Version}";
 $serverName = "vaf-root-sqla3";
 $serverPassword = "#{Databasepassord}";
 // Change password with #{Databasepassord} before a commit
-$connectionOptions = array("Database"=>"Pureservice3", "Uid"=>"SPlese", "PWD"=>"Tpiwtlteraa1");
+$connectionOptions = array("Database"=>"Pureservice3", "Uid"=>"SPlese", "PWD"=>"#{Databasepassord}");
 
  //Establishes the connection
 $connect = sqlsrv_connect($serverName, $connectionOptions);
